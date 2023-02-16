23baad5d9733ad9658429fe92c4dd471623f3f16
gharbaoui <mohamed9gharbaoui@gmail.com>
2023-02-16 20:47:07 +0100

    add execute function and move to green phase

--- lib/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart
+++ lib/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart
@@ -1,7 +1,14 @@
+import 'package:dartz/dartz.dart';
+import 'package:number_trivia/core/error/failures.dart';
+import 'package:number_trivia/features/number_trivia/domain/entities/number_trivia.dart';
 import 'package:number_trivia/features/number_trivia/domain/repositories/number_trivia_repository.dart';
 
 class GetConcreteNumberTrivia {
   final NumberTriviaRepository repository;
 
   const GetConcreteNumberTrivia(this.repository);
+
+  Future<Either<Failure, NumberTrivia>> execute({required int number}) {
+    return repository.getConcreteNumberTrivia(number);
+  }
 }
