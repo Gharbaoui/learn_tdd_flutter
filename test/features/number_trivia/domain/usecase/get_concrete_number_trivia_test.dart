d455c0c525425e2e74409dac66db2172d1847c01
gharbaoui <mohamed9gharbaoui@gmail.com>
2023-02-16 20:08:45 +0100

    add a simple mock for repository because it will be used by getConcreteNumberTrivia later

--- test/features/number_trivia/domain/usecase/get_concrete_number_trivia_test.dart
+++ test/features/number_trivia/domain/usecase/get_concrete_number_trivia_test.dart
@@ -1,1 +1,7 @@
+import 'package:mocktail/mocktail.dart';
+import 'package:number_trivia/features/number_trivia/domain/repositories/number_trivia_repository.dart';
+
+class MockNumberTriviaRepository extends Mock
+    implements NumberTriviaRepository {}
+
 void main() {}
