5acd0da07b3ca174063c1c2f6417663e04a88151
gharbaoui <mohamed9gharbaoui@gmail.com>
2023-02-16 20:37:41 +0100

    arrange part of mocking

--- test/features/number_trivia/domain/usecase/get_concrete_number_trivia_test.dart
+++ test/features/number_trivia/domain/usecase/get_concrete_number_trivia_test.dart
@@ -1,4 +1,6 @@
+import 'package:dartz/dartz.dart';
 import 'package:mocktail/mocktail.dart';
+import 'package:number_trivia/features/number_trivia/domain/entities/number_trivia.dart';
 import 'package:number_trivia/features/number_trivia/domain/repositories/number_trivia_repository.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:number_trivia/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart';
@@ -7,12 +9,23 @@ class MockNumberTriviaRepository extends Mock
     implements NumberTriviaRepository {}
 
 void main() {
-  MockNumberTriviaRepository mockNumberTriviaRepository;
-  GetConcreteNumberTrivia getConcreteNumberTrivia;
+  MockNumberTriviaRepository mockNumberTriviaRepository =
+      MockNumberTriviaRepository();
+  GetConcreteNumberTrivia getConcreteNumberTrivia =
+      GetConcreteNumberTrivia(mockNumberTriviaRepository);
 
   setUp(() {
     mockNumberTriviaRepository = MockNumberTriviaRepository();
     getConcreteNumberTrivia =
         GetConcreteNumberTrivia(mockNumberTriviaRepository);
+  });
+
+  group('get concrete number trivia', () {
+    const int tNumber = 1;
+    const NumberTrivia tNumberTrivia = NumberTrivia(text: '', number: tNumber);
+    test('get number from repository', () async {
+      when(() => mockNumberTriviaRepository.getConcreteNumberTrivia(any()))
+          .thenAnswer((_) async => const Right(tNumberTrivia));
+    });
   });
 }
