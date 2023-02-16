cbd34d127d7b0b2b109a4693eeaf9b8d9b4730bd
gharbaoui <mohamed9gharbaoui@gmail.com>
2023-02-16 20:24:12 +0100

    fix red phase

--- test/features/number_trivia/domain/usecase/get_concrete_number_trivia_test.dart
+++ test/features/number_trivia/domain/usecase/get_concrete_number_trivia_test.dart
@@ -1,16 +1,18 @@
 import 'package:mocktail/mocktail.dart';
 import 'package:number_trivia/features/number_trivia/domain/repositories/number_trivia_repository.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:number_trivia/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart';
 
 class MockNumberTriviaRepository extends Mock
     implements NumberTriviaRepository {}
 
 void main() {
-  MockNumberTriviaRepository mockNumberTriviaRepository = MockNumberTriviaRepository();
-  GetConcreteNumberTrivia getConcreteNumberTrivia(mockNumberTriviaRepository);
+  MockNumberTriviaRepository mockNumberTriviaRepository;
+  GetConcreteNumberTrivia getConcreteNumberTrivia;
 
   setUp(() {
     mockNumberTriviaRepository = MockNumberTriviaRepository();
-    getConcreteNumberTrivia = mockNumberTriviaRepository;
+    getConcreteNumberTrivia =
+        GetConcreteNumberTrivia(mockNumberTriviaRepository);
   });
 }
