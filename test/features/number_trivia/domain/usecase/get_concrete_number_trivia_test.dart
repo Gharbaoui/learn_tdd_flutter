7e6dd3ea360da0d885dbcc55d1acd9f473676866
gharbaoui <mohamed9gharbaoui@gmail.com>
2023-02-16 20:18:35 +0100

    get concrete number trivia will use repository and i'll pass it in the constructor, yes i'm using red-green-refactor

--- test/features/number_trivia/domain/usecase/get_concrete_number_trivia_test.dart
+++ test/features/number_trivia/domain/usecase/get_concrete_number_trivia_test.dart
@@ -1,7 +1,16 @@
 import 'package:mocktail/mocktail.dart';
 import 'package:number_trivia/features/number_trivia/domain/repositories/number_trivia_repository.dart';
+import 'package:flutter_test/flutter_test.dart';
 
 class MockNumberTriviaRepository extends Mock
     implements NumberTriviaRepository {}
 
-void main() {}
+void main() {
+  MockNumberTriviaRepository mockNumberTriviaRepository = MockNumberTriviaRepository();
+  GetConcreteNumberTrivia getConcreteNumberTrivia(mockNumberTriviaRepository);
+
+  setUp(() {
+    mockNumberTriviaRepository = MockNumberTriviaRepository();
+    getConcreteNumberTrivia = mockNumberTriviaRepository;
+  });
+}
