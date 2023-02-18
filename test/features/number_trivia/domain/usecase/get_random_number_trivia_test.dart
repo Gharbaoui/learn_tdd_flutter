536bb5e4bff1a98b8ec80d7a842a65106ea7add2
gharbaoui <mohamed9gharbaoui@gmail.com>
2023-02-18 19:45:04 +0100

    add test for get random number trivia

---
+++ test/features/number_trivia/domain/usecase/get_random_number_trivia_test.dart
@@ -0,0 +1,33 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:number_trivia/features/number_trivia/domain/entities/number_trivia.dart';
+import 'package:number_trivia/features/number_trivia/domain/repositories/number_trivia_repository.dart';
+import 'package:number_trivia/features/number_trivia/domain/usecases/get_random_number_trivia.dart';
+
+class MockNumberTriviaRepository extends Mock
+    implements NumberTriviaRepository {}
+
+void main() {
+  const NumberTrivia numberTrivia = NumberTrivia(text: '', number: 1);
+
+  MockNumberTriviaRepository repository = MockNumberTriviaRepository();
+  GetRandomNumberTrivia usecase = GetRandomNumberTrivia(repository);
+
+  setUp(() {
+    repository = MockNumberTriviaRepository();
+    usecase = GetRandomNumberTrivia(repository);
+  });
+
+  test('should get random number trivia from the repository', () async {
+    when(() => repository.getRandomNumberTrivia())
+        .thenAnswer((_) async => const Right(numberTrivia));
+
+    final result = await usecase(NoParams());
+
+    expect(result, const Right(numberTrivia));
+
+    verify(() => repository.getRandomNumberTrivia()).called(1);
+    verifyNoMoreInteractions(repository);
+  });
+}
