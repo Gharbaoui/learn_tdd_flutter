4ca0e647edb4d39df94a4489615d17eed43d9f8b
gharbaoui <mohamed9gharbaoui@gmail.com>
2023-02-22 09:32:31 +0100

    add fromJson

--- lib/features/number_trivia/data/models/number_trivia_model.dart
+++ lib/features/number_trivia/data/models/number_trivia_model.dart
@@ -3,4 +3,11 @@ import 'package:number_trivia/features/number_trivia/domain/entities/number_triv
 class NumberTriviaModel extends NumberTrivia {
   const NumberTriviaModel({required int number, required String text})
       : super(number: number, text: text);
+
+  factory NumberTriviaModel.fromJson(Map<String, dynamic> json) {
+    return NumberTriviaModel(
+      number: (json['number'] as num).toInt(),
+      text: json['text'],
+    );
+  }
 }
