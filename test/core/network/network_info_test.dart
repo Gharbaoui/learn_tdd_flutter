b0e3de5d53d1313435cf103790d5f78780b77d1a
gharbaoui <mohamed9gharbaoui@gmail.com>
2023-02-25 08:52:51 +0100

    test network access with connictivity_plus

--- test/core/network/network_info_test.dart
+++ test/core/network/network_info_test.dart
@@ -1,18 +1,29 @@
-import 'package:data_connection_checker/data_connection_checker.dart';
+import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
 import 'package:number_trivia/core/network/network_info.dart';
 
-class MockDataConnectionChecker extends Mock implements DataConnectionChecker {}
+class MockConnectivity extends Mock implements Connectivity {}
 
 void main() {
-  MockDataConnectionChecker mockDataConnectionChecker =
-      MockDataConnectionChecker();
+  MockConnectivity mockConnectivity = MockConnectivity();
 
-  NetworkInfoImpl networkInfoImpl = NetworkInfoImpl(mockDataConnectionChecker);
+  NetworkInfoImpl networkInfo = NetworkInfoImpl(mockConnectivity);
 
   setUp(() {
-    mockDataConnectionChecker = MockDataConnectionChecker();
-    networkInfoImpl = NetworkInfoImpl(mockDataConnectionChecker);
+    mockConnectivity = MockConnectivity();
+    networkInfo = NetworkInfoImpl(mockConnectivity);
+  });
+
+  group('isConnected', () {
+    test('should return true if we are connected to throgh mobile data',
+        () async {
+      when(() => mockConnectivity.checkConnectivity())
+          .thenAnswer((_) async => ConnectivityResult.mobile);
+      final result = await networkInfo.isConnected;
+
+      verify(() => mockConnectivity.checkConnectivity()).called(1);
+      expect(result, true);
+    });
   });
 }
