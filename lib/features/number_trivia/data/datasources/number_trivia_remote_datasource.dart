920472fb635ae3e8be1afb0b7f1f2f9654f15313
gharbaoui <mohamed9gharbaoui@gmail.com>
2023-02-27 08:50:53 +0100

    make the test pass

--- lib/features/number_trivia/data/datasources/number_trivia_remote_datasource.dart
+++ lib/features/number_trivia/data/datasources/number_trivia_remote_datasource.dart
@@ -1,4 +1,5 @@
 import 'package:number_trivia/features/number_trivia/data/models/number_trivia_model.dart';
+import 'package:http/http.dart' as http;
 
 abstract class NumberTriviaRemoteDataSource {
   /// Calls the http://numbersapi.com/{number} endpoint
@@ -10,4 +11,23 @@ abstract class NumberTriviaRemoteDataSource {
   ///
   /// Throw a [ServerException] for all error codes
   Future<NumberTriviaModel> getRandomNumberTrivia();
+}
+
+class NumberTriviaRemoteDataSourceImpl implements NumberTriviaRemoteDataSource {
+  final http.Client client;
+
+  const NumberTriviaRemoteDataSourceImpl(this.client);
+
+  @override
+  Future<NumberTriviaModel> getConcreteNumberTrivia(int number) async {
+    await client.get(
+      Uri.parse('http://numbersapi.com/$number'),
+      headers: {'Content-Type': 'application/json'},
+    );
+  }
+
+  @override
+  Future<NumberTriviaModel> getRandomNumberTrivia() {
+    throw UnimplementedError();
+  }
 }
