ef9adeafd2721e4b975299cf9a32070701397d04
gharbaoui <mohamed9gharbaoui@gmail.com>
2023-02-28 09:52:36 +0100

    negative numbers are invalid also

--- lib/core/utils/input_converter.dart
+++ lib/core/utils/input_converter.dart
@@ -4,7 +4,9 @@ import 'package:number_trivia/core/error/failures.dart';
 class InputConverter {
   Either<Failure, int> stringToUnsignedInt(String str) {
     try {
-      return Right(int.parse(str));
+      final integer = int.parse(str);
+      if (integer < 0) throw const FormatException();
+      return Right(integer);
     } on FormatException {
       return Left(InvalidInputFailure());
     }
