13885cc1138de02b9cfcbe82836c650f7b8d3327
gharbaoui <mohamed9gharbaoui@gmail.com>
2023-03-01 10:37:37 +0100

    our bloc will depend on
    GetConcreteNumberTrivia
    GetRandomNumberTrivia
    Input Converter
    
    we need Input Converter because UI will send us
    number as string and we concrete and random work
    with ints

---
+++ lib/features/number_trivia/presentation/bloc/number_trivia_bloc.dart
@@ -0,0 +1,18 @@
+import 'package:bloc/bloc.dart';
+import 'package:number_trivia/core/utils/input_converter.dart';
+import 'package:number_trivia/features/number_trivia/presentation/bloc/number_trivia_event.dart';
+import 'package:number_trivia/features/number_trivia/presentation/bloc/number_trivia_state.dart';
+import 'package:number_trivia/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart';
+import 'package:number_trivia/features/number_trivia/domain/usecases/get_random_number_trivia.dart';
+
+class NumberTriviaBloc extends Bloc<NumberTriviaEvent, NumberTriviaState> {
+  final GetConcreteNumberTrivia getConcreteNumberTrivia;
+  final GetRandomNumberTrivia getRandomNumberTrivia;
+  final InputConverter inputConverter;
+
+  NumberTriviaBloc({
+    required this.getConcreteNumberTrivia,
+    required this.getRandomNumberTrivia,
+    required this.inputConverter,
+  }) : super(EmptyNumberTriviaState());
+}
