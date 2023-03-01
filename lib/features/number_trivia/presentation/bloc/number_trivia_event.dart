c3906a3e7235ede606b2df85d6a2e617ac258da8
gharbaoui <mohamed9gharbaoui@gmail.com>
2023-03-01 10:36:33 +0100

    these are events that will be used by our UI to trigger state changes

---
+++ lib/features/number_trivia/presentation/bloc/number_trivia_event.dart
@@ -0,0 +1,21 @@
+import 'package:equatable/equatable.dart';
+import 'package:flutter/foundation.dart';
+
+@immutable
+abstract class NumberTriviaEvent extends Equatable {
+  const NumberTriviaEvent();
+}
+
+class GetNumberTriviaForConcrete extends NumberTriviaEvent {
+  final String numberString;
+
+  const GetNumberTriviaForConcrete(this.numberString);
+
+  @override
+  List<Object?> get props => [numberString];
+}
+
+class GetNumberTriviaForRandom extends Equatable {
+  @override
+  List<Object?> get props => [];
+}
