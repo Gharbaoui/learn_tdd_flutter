6c3d76e5d6453002ba98f7731f22a3bfc62af46c
gharbaoui <mohamed9gharbaoui@gmail.com>
2023-03-02 10:30:26 +0100

    satisfy the invalid input test

--- lib/features/number_trivia/presentation/bloc/number_trivia_bloc.dart
+++ lib/features/number_trivia/presentation/bloc/number_trivia_bloc.dart
@@ -1,9 +1,16 @@
 import 'package:bloc/bloc.dart';
+import 'package:dartz/dartz.dart';
+import 'package:number_trivia/core/error/failures.dart';
 import 'package:number_trivia/core/utils/input_converter.dart';
 import 'package:number_trivia/features/number_trivia/presentation/bloc/number_trivia_event.dart';
 import 'package:number_trivia/features/number_trivia/presentation/bloc/number_trivia_state.dart';
 import 'package:number_trivia/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart';
 import 'package:number_trivia/features/number_trivia/domain/usecases/get_random_number_trivia.dart';
+
+const SERVER_FAILURE_MESSAGE = 'Server Failure';
+const CACHE_FAILURE_MESSAGE = 'Cache Failure';
+const INVALID_INPUT_FAILURE_MESSAGE =
+    'Invalid Input - the number should be >= 0';
 
 class NumberTriviaBloc extends Bloc<NumberTriviaEvent, NumberTriviaState> {
   final GetConcreteNumberTrivia getConcreteNumberTrivia;
@@ -16,7 +23,12 @@ class NumberTriviaBloc extends Bloc<NumberTriviaEvent, NumberTriviaState> {
     required this.inputConverter,
   }) : super(EmptyNumberTriviaState()) {
     on<GetNumberTriviaForConcrete>((event, emit) {
-      inputConverter.stringToUnsignedInt(event.numberString);
+      final converterResult =
+          inputConverter.stringToUnsignedInt(event.numberString);
+      if (converterResult.isLeft()) {
+        emit(ErrorNumberTriviaState(
+            errorMessage: INVALID_INPUT_FAILURE_MESSAGE));
+      }
     });
   }
 }
