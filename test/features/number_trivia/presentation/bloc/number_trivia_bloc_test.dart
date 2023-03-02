68333f1c6a6091332116beed86c24faffd12c8a8
gharbaoui <mohamed9gharbaoui@gmail.com>
2023-03-02 09:41:33 +0100

    test for initial state

--- test/features/number_trivia/presentation/bloc/number_trivia_bloc_test.dart
+++ test/features/number_trivia/presentation/bloc/number_trivia_bloc_test.dart
@@ -1,7 +1,9 @@
+import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
 import 'package:number_trivia/core/utils/input_converter.dart';
 import 'package:number_trivia/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart';
 import 'package:number_trivia/features/number_trivia/domain/usecases/get_random_number_trivia.dart';
+import 'package:number_trivia/features/number_trivia/presentation/bloc/bloc.dart';
 
 class MockGetConcreteNumberTrivia extends Mock
     implements GetConcreteNumberTrivia {}
@@ -10,4 +12,24 @@ class MockGetRandomNumberTrivia extends Mock implements GetRandomNumberTrivia {}
 
 class MockInputConverter extends Mock implements InputConverter {}
 
-void main() {}
+void main() {
+  late MockGetConcreteNumberTrivia concrete;
+  late MockGetRandomNumberTrivia random;
+  late MockInputConverter inputConverter;
+  late NumberTriviaBloc numberTriviaBloc;
+
+  setUp(() {
+    concrete = MockGetConcreteNumberTrivia();
+    random = MockGetRandomNumberTrivia();
+    inputConverter = MockInputConverter();
+    numberTriviaBloc = NumberTriviaBloc(
+      getConcreteNumberTrivia: concrete,
+      getRandomNumberTrivia: random,
+      inputConverter: inputConverter,
+    );
+  });
+
+  test('initialState should be empty', () {
+    expect(numberTriviaBloc.state, EmptyNumberTriviaState());
+  });
+}
