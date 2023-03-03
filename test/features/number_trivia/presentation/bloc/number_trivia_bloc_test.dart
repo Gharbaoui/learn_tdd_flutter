09edee7a3d4537d483f077c657a55771cc652602
gharbaoui <mohamed9gharbaoui@gmail.com>
2023-03-03 10:46:35 +0100

    if we could not get the data from cache we should emit proper error state

--- test/features/number_trivia/presentation/bloc/number_trivia_bloc_test.dart
+++ test/features/number_trivia/presentation/bloc/number_trivia_bloc_test.dart
@@ -2,6 +2,7 @@ import 'package:bloc_test/bloc_test.dart';
 import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
+import 'package:number_trivia/core/error/failures.dart';
 import 'package:number_trivia/core/utils/input_converter.dart';
 import 'package:number_trivia/features/number_trivia/domain/entities/number_trivia.dart';
 import 'package:number_trivia/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart';
@@ -104,6 +105,22 @@ void main() {
       expect: () => [
         LoadingNumberTriviaState(),
         LoadedNumberTriviaState(numberTrivia: tNumberTrivia),
+      ],
+    );
+
+    blocTest(
+      'should emit [Loading, Error] when data is gotten unsuccessfull with cache error message',
+      setUp: () {
+        when(() => inputConverter.stringToUnsignedInt(any()))
+            .thenReturn(const Right(tNumberParsed));
+        when(() => concrete(any()))
+            .thenAnswer((_) => Future.value(Left(CacheFailure())));
+      },
+      build: () => numberTriviaBloc,
+      act: (bloc) => bloc.add(const GetNumberTriviaForConcrete(tNumberString)),
+      expect: () => [
+        LoadingNumberTriviaState(),
+        ErrorNumberTriviaState(errorMessage: CACHE_FAILURE_MESSAGE),
       ],
     );
   });
